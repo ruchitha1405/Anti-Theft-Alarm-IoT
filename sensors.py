4943ba8a068caa2c882062443502f6977c57e333
Bhargavi <bhargavi.kurukunda@students.iiit.ac.in>
2021-11-19 20:53:59 +0530

    om2m py code to create rooms for data

--- sensors.py
+++ sensors.py
@@ -4,7 +4,7 @@ uri_cse = "http://127.0.0.1:8080/~/in-cse/in-name"
 
 # ========================== room for PIR sensor =====================================
 
-ae1 = "pir_sensor"
+ae1 = "Motion"
 cnt1 = "node1"
 
 create_ae(uri_cse, ae1)
@@ -17,7 +17,7 @@ create_data_cin(uri_cnt1, "random_value")
 
 # ========================== room for HC-SR04 sensor ===================================
 
-ae2 = "hcsr04_sensor"
+ae2 = "Distance"
 cnt2 = "node2"
 
 create_ae(uri_cse, ae2)
@@ -28,9 +28,9 @@ create_cnt(uri_ae2, cnt2)
 uri_cnt2 = uri_ae2 + "/" + cnt2
 create_data_cin(uri_cnt2, "random_value")
 
-# =========================== room for DHT11 sensor =====================================
+# =========================== room for temperature =====================================
 
-ae3 = "DHT11_sensor"
+ae3 = "Temperature"
 cnt3 = "node3"
 
 create_ae(uri_cse, ae3)
@@ -40,4 +40,17 @@ create_cnt(uri_ae3, cnt3)
 
 uri_cnt3 = uri_ae3 + "/" + cnt3
 create_data_cin(uri_cnt3, "random_value")
+
+# =========================== room for humidity =====================================
+
+ae4 = "Humidity"
+cnt4 = "node4"
+
+create_ae(uri_cse, ae4)
+
+uri_ae4 = uri_cse + "/" + ae4
+create_cnt(uri_ae4, cnt4)
+
+uri_cnt4 = uri_ae4 + "/" + cnt4
+create_data_cin(uri_cnt4, "random_value")
 
