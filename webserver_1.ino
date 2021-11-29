ad1e18f20179c685fbd4a7b073935dcc618dda6a
girija2020 <82602432+girija2020@users.noreply.github.com>
2021-11-29 18:28:19 +0530

    Create webserver_1.ino

--- webserver_1.ino
+++ webserver_1.ino
@@ -351,10 +351,8 @@ ptr +="</script>\n";
   else
   {ptr +="<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";}
 */
- /* ptr += "<button class=\"control-btn\">LED</button>\n";
-  ptr += "<button class=\"control-btn\">Buzzer</button>\n";*/
-    ptr +="<a href=\"/monitorled\"><button class=\"control-btn\">LED</button></a>\n";
-ptr +="<a href=\"/monitorbuzzer\"><button class=\"control-btn\">Buzzer</button></a>\n";
+  ptr += "<button class=\"control-btn\">LED</button>\n";
+  ptr += "<button class=\"control-btn\">Buzzer</button>\n";
   ptr += "</div>\n";
   
   
@@ -383,31 +381,6 @@ void handle_monitor(){
 void handle_NotFound(){
   server.send(404, "text/plain", "Not found");
 }
-////////extra
-void handle_monitorled(){
-    if(led_status=="ON"){
-          digitalWrite(motionLed, LOW);
-          digitalWrite(fireLed, LOW);
-          led_status=="OFF";
-      }
-      else {
-          digitalWrite(motionLed, HIGH);
-          led_status=="ON";
-      }
-  server.send(200, "text/html", SendHTML_monitor(temperature,humidity, pressure,led_status, buzzer_status, distance, motion));
-  }
-  void handle_monitorbuzzer(){
-      if(buzzer_status=="ON"){
-          digitalWrite(buzzer, LOW);
-          buzzer_status=="OFF";
-      }
-      else {
-          digitalWrite(buzzer, HIGH);
-          buzzer_status=="ON";
-      }
-  server.send(200, "text/html", SendHTML_monitor(temperature,humidity, pressure,led_status, buzzer_status, distance, motion));
-  }
-  ////////////////////
 //**************HANDLE FUNCTIONS END*************************
 
 void setup() {
@@ -446,11 +419,7 @@ void setup() {
   server.on("/login", handle_login);
   server.on("/monitor", handle_monitor);
   server.onNotFound(handle_NotFound);
-    
-    /////extra
- server.on("/monitorled", handle_monitorled);
- server.on("/monitorbuzzer", handle_monitorbuzzer);
-///////
+
   server.begin();
   Serial.println("HTTP server started");
 
@@ -459,7 +428,10 @@ void setup() {
 
 void loop() {
   server.handleClient();
-  led_status = String(0);
+  led_status = String("OFF");
+  digitalWrite(motionLed, LOW);
+  digitalWrite(fireLed, LOW);
+  digitalWrite(buzzer, LOW);
   int authentication = 0;
   username = "";
   password = "";
@@ -502,16 +474,11 @@ void loop() {
     digitalWrite(buzzer, HIGH);
     while(1)
     {
-    Serial.println("Motion detected. If this is something you know please press y");
-    if(Serial.available() > 0)
-    {
-      String yes;
-      yes = Serial.readString();
-      if(yes == "y\n")
-      {
+    Serial.println("Motion detected. If this is something you know please open the website");
+        if(led_status = "OFF"){
+
         led_status = String(0);
-        continue;
-      }
+        break;;
     }
     delay(1000);
   }
@@ -529,12 +496,14 @@ void loop() {
   if (t > 50)
   {
     digitalWrite(fireLed, HIGH);
-    led_status = String(1);
+    led_status = String("ON");
     digitalWrite(buzzer, HIGH);
   }
 
   while(authentication == 1)
   {
+    digitalWrite(fireLed, LOW);
+    digitalWrite(buzzer, LOW);
     isDetected = digitalRead(pirPin);
     motion = String(isDetected);
     if(isDetected)
@@ -549,7 +518,7 @@ void loop() {
     if (t > 50)
     {
       digitalWrite(fireLed, HIGH);
-      led_status = String(1);
+      led_status = String("ON");
       digitalWrite(buzzer, HIGH);
     }
     
